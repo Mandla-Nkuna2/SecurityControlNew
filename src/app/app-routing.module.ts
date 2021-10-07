a4ca600234d62b2ee6a544590db437c64761e07a
algorsnipes@gmail.com <algorsnipes@gmail.com>
2021-10-07 11:01:50 +0200

    removed old form files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,20 +8,20 @@ const routes: Routes = [
   { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule) },
   { path: 'registration', loadChildren: () => import('./pages/registration/registration.module').then(m => m.RegistrationPageModule) },
   { path: 'forgot-password', loadChildren: () => import('./pages/forgot-password/forgot-password.module').then(m => m.ForgotPasswordPageModule) },
-  { path: '', loadChildren: () => import('./pages/menu/menu.module').then(m => m.MenuPageModule), canActivate: [AuthGuardService] },   { path: 'ncr', loadChildren: () => import('./pages/ncr/ncr.module').then(m => m.NcrPageModule) },
-
-  
+  { path: '', loadChildren: () => import('./pages/menu/menu.module').then(m => m.MenuPageModule), canActivate: [AuthGuardService] }, { path: 'ncr', loadChildren: () => import('./pages/ncr/ncr.module').then(m => m.NcrPageModule) },
 
 
- 
 
- 
 
-  
 
-  
 
- 
+
+
+
+
+
+
+
 
 ];
 
