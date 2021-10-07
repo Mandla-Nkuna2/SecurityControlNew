4ac34b91fb3139cb939d301455021cd4a76651a6
KathrynvdP <kathryn@innovativethinking.co.za>
2021-10-07 12:17:10 +0200

    Validate subscriptions cloud function

--- src/app/pages/memberships-app/memberships-app.page.ts
+++ src/app/pages/memberships-app/memberships-app.page.ts
@@ -90,9 +90,9 @@ export class MembershipsAppPage implements OnInit {
           date: moment(new Date()).format('YYYY/MM/DD'),
           companyId: newUser.companyId,
           number: 1,
+          type: 'App',
           transaction: Object.assign({}, newObj)
         });
-        //this.membershipService.setAppSubscriptions(newUser.companyId, sub).then(() => {
         this.membershipService.updateCompany(user, {
           accessType: this.chosenItem.title,
           access: true
@@ -102,7 +102,6 @@ export class MembershipsAppPage implements OnInit {
             this.navController.navigateRoot('menu/form-menu');
           })
         })
-        //})
       })
     });
   }
